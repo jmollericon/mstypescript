eade6f4998db42ded0787888ab07f69682cf371a
Jorge Mollericon <jlmollericon@gmail.com>
2022-05-17 12:15:23 -0400

    ⚡️ Any and unknown types in TypeScript

--- build/module01.js
+++ build/module01.js
@@ -31,3 +31,36 @@ var ContractStatus;
 let employeeStatus = ContractStatus.Temp;
 console.log(employeeStatus);
 console.log(ContractStatus[employeeStatus]);
+// Any type
+let randomValue = 10;
+randomValue = 'Mateo'; // OK
+randomValue = true; // OK
+console.log(randomValue.name); // Logs "undefined" to the console
+//randomValue();                  // Returns "randomValue is not a function" error
+//randomValue.toUpperCase();      // Returns "randomValue is not a function" error
+// Unknown type
+let randomValue2 = 10;
+randomValue2 = true;
+randomValue2 = 'Mateo';
+// console.log(randomValue2.name);  // Error: Object is of type unknown
+// randomValue2();                  // Error: Object is of type unknown
+// randomValue2.toUpperCase();      // Error: Object is of type unknown
+// Type assertion
+let randomValue3 = 10;
+randomValue3 = true;
+randomValue3 = 'Mateo';
+if (typeof randomValue3 === "string") {
+    console.log(randomValue3.toUpperCase()); //* Returns MATEO to the console.
+}
+else {
+    console.log("Error - A string was expected here."); //* Returns an error message.
+}
+// Type guards
+/*
+  string	    typeof s === "string"
+  number	    typeof n === "number"
+  boolean	    typeof b === "boolean"
+  undefined	  typeof undefined === "undefined"
+  function	  typeof f === "function"
+  array	      Array.isArray(a)
+*/
