09cd7ecf4feabb8e2e5181e6e801fe9b7fc40bca
Jorge Mollericon <jlmollericon@gmail.com>
2022-05-17 12:07:10 -0400

    ⚡️ Primitive types in TypeScript

--- build/module01.js
+++ build/module01.js
@@ -3,3 +3,20 @@ function addNumbers(x, y) {
     return x + y;
 }
 console.log(addNumbers(3, 6));
+// Boolean type
+let flag;
+let yes = true;
+let no = false;
+// Number and BigInteger types
+let x;
+let y = 0;
+let z = 123.456;
+let big = 10000000000000000000000; //100n;
+//String type
+let s;
+let empty = "";
+let abc = 'abc';
+let firstName = "Mateo";
+let sentence = `My name is ${firstName}.
+  I am new to TypeScript.`;
+console.log(sentence);
--- module01.ts
+++ module01.ts
@@ -2,3 +2,24 @@ function addNumbers(x: number, y: number) {
   return x + y;
 }
 console.log(addNumbers(3, 6));
+
+// Boolean type
+let flag: boolean;
+let yes = true;
+let no = false;
+
+// Number and BigInteger types
+let x: number;
+let y = 0;
+let z: number = 123.456;
+// let big: bigint = 100n;
+
+//String type
+let s: string;
+let empty = "";
+let abc = 'abc';
+
+let firstName: string = "Mateo";
+let sentence: string = `My name is ${firstName}.
+  I am new to TypeScript.`;
+console.log(sentence);
