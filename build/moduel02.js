3bc07495947412bb82a807fd29072abf1e17e9f2
Jorge Mollericon <jlmollericon@gmail.com>
2022-05-17 12:20:15 -0400

    ⚡️ Union and intersection types in TypeScript

--- build/moduel02.js
+++ build/moduel02.js
@@ -32,15 +32,15 @@ let randomValue = 10;
 randomValue = 'Mateo'; // OK
 randomValue = true; // OK
 console.log(randomValue.name); // Logs "undefined" to the console
-//randomValue();                  // Returns "randomValue is not a function" error
-//randomValue.toUpperCase();      // Returns "randomValue is not a function" error
+randomValue(); // Returns "randomValue is not a function" error
+randomValue.toUpperCase(); // Returns "randomValue is not a function" error
 // Unknown type
 let randomValue2 = 10;
 randomValue2 = true;
 randomValue2 = 'Mateo';
-// console.log(randomValue2.name);  // Error: Object is of type unknown
-// randomValue2();                  // Error: Object is of type unknown
-// randomValue2.toUpperCase();      // Error: Object is of type unknown
+console.log(randomValue2.name); // Error: Object is of type unknown
+randomValue2(); // Error: Object is of type unknown
+randomValue2.toUpperCase(); // Error: Object is of type unknown
 // Type assertion
 let randomValue3 = 10;
 randomValue3 = true;
@@ -60,3 +60,37 @@ else {
   function	  typeof f === "function"
   array	      Array.isArray(a)
 */
+// Union and intersection types in TypeScript
+// ==========================================
+// Union types
+let multiType;
+multiType = 20; //* Valid
+multiType = true; //* Valid
+multiType = "twenty"; //* Invalid
+function add(x, y) {
+    if (typeof x === 'number' && typeof y === 'number') {
+        return x + y;
+    }
+    if (typeof x === 'string' && typeof y === 'string') {
+        return x.concat(y);
+    }
+    throw new Error('Parameters must be numbers or strings');
+}
+console.log(add('one', 'two')); //* Returns "onetwo"
+console.log(add(1, 2)); //* Returns 3
+console.log(add('one', 2)); //* Returns error
+let newManager = {
+    employeeID: 12345,
+    age: 34,
+    stockPlan: true
+};
+let myResult;
+myResult = "incomplete"; //* Valid
+myResult = "pass"; //* Valid
+myResult = "failure"; //* Invalid
+let diceRoll;
+diceRoll = 1; //* Valid
+diceRoll = 2; //* Valid
+diceRoll = 7; //* Invalid
+// 
+// 
--- moduel02.ts
+++ moduel02.ts
@@ -38,17 +38,17 @@ randomValue = 'Mateo';   // OK
 randomValue = true;      // OK
 
 console.log(randomValue.name);  // Logs "undefined" to the console
-//randomValue();                  // Returns "randomValue is not a function" error
-//randomValue.toUpperCase();      // Returns "randomValue is not a function" error
+randomValue();                  // Returns "randomValue is not a function" error
+randomValue.toUpperCase();      // Returns "randomValue is not a function" error
 
 // Unknown type
 let randomValue2: unknown = 10;
 randomValue2 = true;
 randomValue2 = 'Mateo';
 
-// console.log(randomValue2.name);  // Error: Object is of type unknown
-// randomValue2();                  // Error: Object is of type unknown
-// randomValue2.toUpperCase();      // Error: Object is of type unknown
+console.log(randomValue2.name);  // Error: Object is of type unknown
+randomValue2();                  // Error: Object is of type unknown
+randomValue2.toUpperCase();      // Error: Object is of type unknown
 
 // Type assertion
 let randomValue3: unknown = 10;
@@ -72,3 +72,58 @@ if (typeof randomValue3 === "string") {
   function	  typeof f === "function"
   array	      Array.isArray(a)
 */
+
+
+// Union and intersection types in TypeScript
+// ==========================================
+
+// Union types
+let multiType: number | boolean;
+multiType = 20;         //* Valid
+multiType = true;       //* Valid
+multiType = "twenty";   //* Invalid
+
+function add(x: number | string, y: number | string) {
+    if (typeof x === 'number' && typeof y === 'number') {
+        return x + y;
+    }
+    if (typeof x === 'string' && typeof y === 'string') {
+        return x.concat(y);
+    }
+    throw new Error('Parameters must be numbers or strings');
+}
+console.log(add('one', 'two'));  //* Returns "onetwo"
+console.log(add(1, 2));          //* Returns 3
+console.log(add('one', 2));      //* Returns error
+
+// Intersection types
+interface Employee {
+  employeeID: number;
+  age: number;
+}
+interface Manager {
+  stockPlan: boolean;
+}
+type ManagementEmployee = Employee & Manager;
+let newManager: ManagementEmployee = {
+  employeeID: 12345,
+  age: 34,
+  stockPlan: true
+};
+
+// Literal types
+type testResult = "pass" | "fail" | "incomplete";
+let myResult: testResult;
+myResult = "incomplete";    //* Valid
+myResult = "pass";          //* Valid
+myResult = "failure";       //* Invalid
+
+type dice = 1 | 2 | 3 | 4 | 5 | 6;
+let diceRoll: dice;
+diceRoll = 1;    //* Valid
+diceRoll = 2;    //* Valid
+diceRoll = 7;    //* Invalid
+
+// 
+
+// 
