1b99287b11e5c5cf8f35b51463b6cd52a498b5a9
Jorge Mollericon <jlmollericon@gmail.com>
2022-05-17 14:49:07 -0400

    ✨ Create functions in TypeScript

---
+++ module04/module04.ts
@@ -0,0 +1,52 @@
+// Develop typed functions by using TypeScript
+// *******************************************
+
+// Create functions in TypeScript
+
+// Named functions
+function addNumbers(x: number, y: number): number {
+  return x + y;
+}
+addNumbers(1, 2);
+
+// Anonymous functions
+
+// This example assigns a function expression to the variable addNumbers. Notice that function appears in place of the function name, making the function anonymous. You can now use this variable to call the function.
+let addNumbersTwo = function (x: number, y: number): number {
+  return x + y;
+};
+addNumbersTwo(1, 2);
+
+// This shows what the named function sum looks like when written as an anonymous function. Notice that the name add has been replaced with function and the function has been implemented as an expression in a variable declaration.
+let total = function (input: number[]): number {
+  let total: number = 0;
+  for (let i = 0; i < input.length; i++) {
+    if (isNaN(input[i])) {
+      continue;
+    }
+    total += Number(input[i]);
+  }
+  return total;
+};
+
+console.log(total([1, 2, 3]));
+
+// Arrow functions
+// Anonymous function
+let addNumbers1 = function (x: number, y: number): number {
+  return x + y;
+};
+
+// Arrow function
+let addNumbers2 = (x: number, y: number): number => x + y;
+
+let total2 = (input: number[]): number => {
+  let total: number = 0;
+  for (let i = 0; i < input.length; i++) {
+    if (isNaN(input[i])) {
+      continue;
+    }
+    total += Number(input[i]);
+  }
+  return total;
+};
