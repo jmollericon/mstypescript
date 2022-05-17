c4e2eaec1adb9503561efd4c811dcd4e8f416c5a
Jorge Mollericon <jlmollericon@gmail.com>
2022-05-17 14:01:19 -0400

    ⚡️ Other ways to use interfaces in Typescript

--- build/module03/module03.js
+++ build/module03/module03.js
@@ -1,6 +1,15 @@
 "use strict";
 // What is an interface
 // You can use interfaces to describe an object, naming and parameterizing the object's types, and to compose existing named object types into new ones.
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 let employee = {
     firstName: "Emil",
     lastName: "Andersson",
@@ -37,3 +46,29 @@ function tooManyScoopsSundae(dessert) {
     }
 }
 console.log(tooManyScoopsSundae({ flavor: 'vanilla', scoops: 5, sauce: 'caramel' }));
+let myIceCream2;
+myIceCream2 = ['chocolate', 'vanilla', 'strawberry'];
+let myStr = myIceCream2[0];
+console.log(myStr);
+// Describe a JavaScript API using an interface
+const fetchURL = 'https://jsonplaceholder.typicode.com/posts';
+function fetchPosts(url) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let response = yield fetch(url);
+        let body = yield response.json();
+        return body;
+    });
+}
+function showPost() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let posts = yield fetchPosts(fetchURL);
+        // Display the contents of the first item in the response
+        let post = posts[0];
+        console.log('Post #' + post.id);
+        // If the userId is 1, then display a note that it's an administrator
+        console.log('Author: ' + (post.userId === 1 ? "Administrator" : post.userId.toString()));
+        console.log('Title: ' + post.title);
+        console.log('Body: ' + post.body);
+    });
+}
+showPost();
--- module03/module03.ts
+++ module03/module03.ts
@@ -81,4 +81,42 @@ function tooManyScoopsSundae(dessert: Sundae) {
 }
 console.log(tooManyScoopsSundae({flavor: 'vanilla', scoops: 5, sauce: 'caramel'}));
 
+// Other ways to use interfaces in Typescript
 
+// Create indexable types
+interface IceCreamArray {
+  [index: number]: string;
+}
+
+let myIceCream2: IceCreamArray;
+myIceCream2 = ['chocolate', 'vanilla', 'strawberry'];
+let myStr: string = myIceCream2[0];
+console.log(myStr);
+
+// Describe a JavaScript API using an interface
+
+const fetchURL = 'https://jsonplaceholder.typicode.com/posts'
+// Interface describing the shape of our json data
+interface Post {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+async function fetchPosts(url: string) {
+    let response = await fetch(url);
+    let body = await response.json();
+    return body as Post[];
+}
+async function showPost() {
+    let posts = await fetchPosts(fetchURL);
+    // Display the contents of the first item in the response
+    let post = posts[0];
+    console.log('Post #' + post.id)
+    // If the userId is 1, then display a note that it's an administrator
+    console.log('Author: ' + (post.userId === 1 ? "Administrator" : post.userId.toString()))
+    console.log('Title: ' + post.title)
+    console.log('Body: ' + post.body)
+}
+
+showPost();
