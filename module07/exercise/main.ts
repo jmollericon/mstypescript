d9b0fca24e71b9aed4d114809ebd152c0a4d1607
Jorge Mollericon <jlmollericon@gmail.com>
2022-05-18 11:10:32 -0400

    ⚡️ implementation of solution of exercices of module07

--- module07/exercise/main.ts
+++ module07/exercise/main.ts
@@ -2,9 +2,11 @@
     Lab Start */
 
 /*  TODO Add the import statement. */
+// import { calculateInterestOnlyLoanPayment, calculateConventionalLoanPayment } from './loan-programs';
+import * as LoanPrograms from './loan-programs.js';
 
 /*  TODO Update the function calls. */
-let interestOnlyPayment = calculateInterestOnlyLoanPayment({principle: 30000, interestRate: 5});
-let conventionalLoanPayment = calculateConventionalLoanPayment({principle: 30000, interestRate: 5, months: 180});
-console.log(interestOnlyPayment);         //* Returns "The interest only loan payment is 125.00" 
+let interestOnlyPayment = LoanPrograms.calculateInterestOnlyLoanPayment({principle: 30000, interestRate: 5});
+let conventionalLoanPayment = LoanPrograms.calculateConventionalLoanPayment({principle: 30000, interestRate: 5, months: 180});
+console.log(interestOnlyPayment);         //* Returns "The interest only loan payment is 125.00"
 console.log(conventionalLoanPayment);     //* Returns "The conventional loan payment is 237.24"
