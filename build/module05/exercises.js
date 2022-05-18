e94e04721273267b71c08deff20214458f24ea18
Jorge Mollericon <jlmollericon@gmail.com>
2022-05-18 09:48:45 -0400

    ✨ solution exercises module 05

--- build/module05/exercises.js
+++ build/module05/exercises.js
@@ -3,8 +3,6 @@
 /*  Module 5: Declare and instantiate classes in TypeScript
     Lab Start  */
 /*  EXERCISE 1 */
-class BuildArray {
-}
 /*  sortDescending is a comparison function that tells the sort method how to sort numbers
   in descending order. */
 let sortDescending = (a, b) => {
@@ -58,3 +56,69 @@ let testArray1 = buildArray(12, 'ascending');
 let testArray2 = buildArray(8, 'descending');
 console.log(testArray1);
 console.log(testArray2);
+class BuildArray {
+    // constructor
+    constructor(items, sortOrder) {
+        // methods
+        this.sortDescending = (a, b) => {
+            if (a > b) {
+                return -1;
+            }
+            else if (b > a) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        };
+        this.sortAscending = (a, b) => {
+            if (a > b) {
+                return 1;
+            }
+            else if (b > a) {
+                return -1;
+            }
+            else {
+                return 0;
+            }
+        };
+        this._items = items;
+        this._sortOrder = sortOrder;
+    }
+    // accessors
+    getItems() {
+        return this._items;
+    }
+    setItems(items) {
+        this._items = items;
+    }
+    getSortOrder() {
+        return this._sortOrder;
+    }
+    setSortOrder(sortOrder) {
+        this._sortOrder = sortOrder;
+    }
+    buildArray() {
+        let randomNumbers = [];
+        let nextNumber;
+        for (let counter = 0; counter < this._items; counter++) {
+            nextNumber = Math.ceil(Math.random() * (100 - 1));
+            if (randomNumbers.indexOf(nextNumber) === -1) {
+                randomNumbers.push(nextNumber);
+            }
+            else {
+                counter--;
+            }
+        }
+        if (this._sortOrder === 'ascending') {
+            return randomNumbers.sort(this.sortAscending);
+        }
+        else {
+            return randomNumbers.sort(this.sortDescending);
+        }
+    }
+}
+const example1 = new BuildArray(12, 'ascending');
+const example2 = new BuildArray(8, 'descending');
+console.log(example1.buildArray());
+console.log(example2.buildArray());
--- module05/exercises.ts
+++ module05/exercises.ts
@@ -5,17 +5,6 @@
 
 /*  EXERCISE 1 */
 
-class BuildArray {
-  // TODO Define the properties
-
-  // TODO Define the constructor
-
-  // TODO Define the accessors
-
-  // TODO Define the methods
-
-}
-
 /*  sortDescending is a comparison function that tells the sort method how to sort numbers
   in descending order. */
 let sortDescending = (a: number, b: number) => {
@@ -67,3 +56,72 @@ let testArray2 = buildArray(8, 'descending');
 console.log(testArray1);
 console.log(testArray2);
 
+type sortOrderType = "ascending" | "descending";
+class BuildArray {
+  // properties
+  private _items: number;
+  private _sortOrder: sortOrderType
+
+  // constructor
+  constructor(items: number, sortOrder: sortOrderType) {
+    this._items = items;
+    this._sortOrder = sortOrder;
+  }
+
+  // accessors
+  getItems(): number {
+    return this._items;
+  }
+  setItems(items: number): void {
+    this._items = items;
+  }
+  getSortOrder(): sortOrderType {
+    return this._sortOrder;
+  }
+  setSortOrder(sortOrder: sortOrderType): void {
+    this._sortOrder = sortOrder;
+  }
+
+  // methods
+  private sortDescending = (a: number, b: number) => {
+    if (a > b) {
+      return -1;
+    } else if (b > a) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+  private sortAscending = (a: number, b: number) => {
+    if (a > b) {
+      return 1;
+    } else if (b > a) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
+  buildArray(): number[] {
+    let randomNumbers: number[] = [];
+    let nextNumber: number;
+    for (let counter = 0; counter < this._items; counter++) {
+      nextNumber = Math.ceil(Math.random() * (100 - 1));
+      if (randomNumbers.indexOf(nextNumber) === -1) {
+        randomNumbers.push(nextNumber);
+      } else {
+        counter--;
+      }
+    }
+    if (this._sortOrder === 'ascending') {
+      return randomNumbers.sort(this.sortAscending);
+    } else {
+      return randomNumbers.sort(this.sortDescending);
+    }
+  }
+}
+
+const example1 = new BuildArray(12, 'ascending');
+const example2 = new BuildArray(8, 'descending');
+
+console.log(example1.buildArray());
+console.log(example2.buildArray());
